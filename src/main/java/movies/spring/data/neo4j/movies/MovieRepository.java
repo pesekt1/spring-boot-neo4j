90a2a2fc287fdfdf480eea544069e5dc69b17f91
Tomas Pesek <pesekt@gmail.com>
2021-11-02 13:48:01 +0100

    first commit

---
+++ src/main/java/movies/spring/data/neo4j/movies/MovieRepository.java
@@ -0,0 +1,17 @@
+package movies.spring.data.neo4j.movies;
+
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * This repository is indirectly used in the {@code movies.spring.data.neo4j.api.MovieController} via a dedicated movie service.
+ * It is not a public interface to indicate that access is either through the rest resources or through the service.
+ */
+interface MovieRepository extends Repository<Movie, String> {
+
+	@Query("MATCH (movie:Movie) WHERE movie.title CONTAINS $title RETURN movie")
+	List<Movie> findSearchResults(@Param("title") String title);
+}
